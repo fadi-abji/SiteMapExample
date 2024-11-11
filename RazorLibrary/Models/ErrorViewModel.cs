360f8bb08179d1af7e3500dace4fbceb0e05aec8
Fadi Abji <fadi@wexer.se>
2024-11-11 13:59:29 +0100

    Razor Library

---
+++ RazorLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RazorLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- SiteMapExample/Sitemap/Providers/ProductSitemapUrlProvider.cs
+++ SiteMapExample/Sitemap/Providers/ProductSitemapUrlProvider.cs
@@ -1,23 +1,23 @@
 using DotnetSitemapGenerator;
-using Microsoft.EntityFrameworkCore;
-using SiteMapExample.Models;
+//using Microsoft.EntityFrameworkCore;
+//using SiteMapExample.Models;
 
 namespace SiteMapExample.Sitemap.Providers;
 
 public class ProductSitemapUrlProvider : ISitemapUrlProvider
 {
-    private readonly Database _db;
+    //private readonly Database _db;
     private readonly LinkGenerator _linkGenerator;
     private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly ILogger<ProductSitemapUrlProvider> _logger;
 
     public ProductSitemapUrlProvider(
-        Database db,
+        //Database db,
         LinkGenerator linkGenerator,
         IHttpContextAccessor httpContextAccessor,
         ILogger<ProductSitemapUrlProvider> logger)
     {
-        _db = db;
+        //_db = db;
         _linkGenerator = linkGenerator;
         _httpContextAccessor = httpContextAccessor;
         _logger = logger;
@@ -26,18 +26,18 @@ public class ProductSitemapUrlProvider : ISitemapUrlProvider
     public async Task<IReadOnlyCollection<SitemapNode>> GetNodes()
     {
         var elements = new List<SitemapNode>();
-        var products = await _db.Products.OrderBy(x => x.Id).ToListAsync();
+        //var products = await _db.Products.OrderBy(x => x.Id).ToListAsync();
 
-        foreach (var product in products)
-        {
-            var url = _linkGenerator.GetUriByPage(
-                _httpContextAccessor.HttpContext!,
-                page: "/Products",
-                values: new { product.Id, product.Slug });
+        //foreach (var product in products)
+        //{
+        //    var url = _linkGenerator.GetUriByPage(
+        //        _httpContextAccessor.HttpContext!,
+        //        page: "/Products",
+        //        values: new { product.Id, product.Slug });
 
-            _logger.LogInformation("Adding product: {URL}", url);
-            elements.Add(new SitemapNode(url));
-        }
+        //    _logger.LogInformation("Adding product: {URL}", url);
+        //    elements.Add(new SitemapNode(url));
+        //}
 
         return elements;
     }
